67cc4b567f78ef8763c3575abfc27f88ed89224c
RayeRen <3150103692@zju.edu.cn>
2018-07-31 20:26:08 +0800

    fix bug when using flask-migrations and mysql-connector together

--- app/settings.py
+++ app/settings.py
@@ -6,7 +6,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_NAME = os.getenv("DB_NAME")
-DB_URI_BASE = 'mysql+mysqlconnector://' + DB_USER + ':' + DB_PASS + '@' + DB_HOST + '/'
+DB_URI_BASE = 'mysql+pymysql://' + DB_USER + ':' + DB_PASS + '@' + DB_HOST + '/'
 
 
 class Config(object):
